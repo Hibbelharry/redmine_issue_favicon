99d166ad99f29c34638bd19c4f30a1437fd281c0
Wilken Haase <w.haase@gambio.de>
2014-10-17 21:08:01 +0200

    First Commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+group :test do
+  gem "yard"
+  gem "simplecov","~> 0.6"
+  gem "simplecov-rcov"
+end
---
+++ assets/javascripts/issue_favicon.js
@@ -0,0 +1,8 @@
+/**
+ * Created by takano on 2014/03/02.
+ */
+var favicon=new Favico({
+    animation:'none',
+    bgColor : '#ff0000',
+    textColor : '#fff'
+});
